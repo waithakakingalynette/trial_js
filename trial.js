a58eb138a7ec578540187b789d3cfac6b1414ab9
waithakakingalynette <warukiralynette@gmail.com>
2023-06-13 21:41:29 +0300

    first commit

---
+++ trial.js
@@ -0,0 +1,27 @@
+var house= new Array(10,20,30,40,50);
+console.log(house)
+
+var house1=new Array(5,6,7,8,9);
+console.log(house1)
+
+var home=new Array("1BHK","2BHK");
+console.log(home)
+
+house1[0] = "1BHK"
+    house1[1] = "2BHK"
+    house1[2] = "3BHK"
+    house1[3] = "4BHK"
+    console.log(house1)
+
+    var house2=["1BHK",2000,"2BHK",1500,"3BHK",2300]
+    console.log(house2);
+
+    var house1=new Array(5,6,7,8,9);
+    house1.push(10);
+    console.log(house1);
+    house1.unshift(4);
+    console.log(house1);
+    house1.shift();
+    console.log(house1);
+    house1.pop();
+    console.log(house1);
